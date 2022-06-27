b61ead990ea3763726272a6b2d2fcc23dff85b3a
rebell218 <88341831+rebell218@users.noreply.github.com>
2022-06-27 23:25:14 +0200

    Update index.svelte

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -2,21 +2,7 @@
 	export const prerender = true;
 </script>
 
-<script>
-    
-    let one, oneOne, oneOneOne, oneTwo, oneTwoOne, oneTwoTwo, oneTwoThree, oneTwoFour, oneTwoFive, oneThree;
-    let two, twoOne, twoTwo, twoThree;
-    let three, threeOne;
-    let four, fourOne, fourTwo;
-    let five, fiveOne;
-    let six;
-    let seven, sevenOne, sevenTwo, sevenThree;
 
-    const goToSection = (section) => {
-        const top = section.getBoundingClientRect().top + window.pageYOffset;
-        window.scrollTo({left: 0, top, behavior: 'smooth'});
-    }
-</script>
 
 <style>
     .constitution {
